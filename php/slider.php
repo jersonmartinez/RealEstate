8c9536fd10272c8c2a0d3e203d150d257cf10f37
rolandbarreto <rolandbarreto19@gmail.com>
2016-12-16 19:39:40 -0600

    translate2

--- php/slider.php
+++ php/slider.php
@@ -5,7 +5,7 @@
       <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
         <div class="sl-slide-inner">
           <div class="bg-img bg-img-10"></div>
-          <h2><a href="#">2 Habitaciones de la cama y 1 sitio del comedor Aparment en venta</a></h2>
+          <h2><a href="#">2 Habitaciones con cama y 1 comedor, apartamento en venta</a></h2>
           <blockquote>              
           <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
           <p>Hasta que extienda el círculo de su compasión a todos los seres vivos, el hombre no encontrará la paz</p>
@@ -17,7 +17,7 @@
       <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
         <div class="sl-slide-inner">
           <div class="bg-img bg-img-2"></div>
-          <h2><a href="#">2 Habitaciones de la cama y 1 sitio del comedor Aparment en venta</a></h2>
+          <h2><a href="#">2 Habitaciones con cama y 1 comedor, apartamento venta</a></h2>
           <blockquote>              
           <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
           <p>Hasta que extienda el círculo de su compasión a todos los seres vivos, el hombre no encontrará la paz</p>
@@ -29,7 +29,7 @@
       <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
         <div class="sl-slide-inner">
           <div class="bg-img bg-img-3"></div>
-          <h2><a href="#">2 Habitaciones de la cama y 1 sitio del comedor Aparment en venta</a></h2>
+          <h2><a href="#">2 Habitaciones con cama y 1 comedor, apartamento en venta</a></h2>
           <blockquote>              
           <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
           <p>Hasta que extienda el círculo de su compasión a todos los seres vivos, el hombre no encontrará la paz</p>
@@ -41,7 +41,7 @@
       <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
         <div class="sl-slide-inner">
           <div class="bg-img bg-img-4"></div>
-          <h2><a href="#">2 Habitaciones de la cama y 1 sitio del comedor Aparment en venta</a></h2>
+          <h2><a href="#">2 Habitaciones con cama y 1 comedor, apartamento en venta</a></h2>
           <blockquote>              
           <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
           <p>Hasta que extienda el círculo de su compasión a todos los seres vivos, el hombre no encontrará la paz</p>
@@ -53,7 +53,7 @@
       <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
         <div class="sl-slide-inner">
           <div class="bg-img bg-img-5"></div>
-          <h2><a href="#">2 Habitaciones de la cama y 1 sitio del comedor Aparment en venta</a></h2>
+          <h2><a href="#">2 Habitaciones con cama y 1 comedor, apartamento en venta</a></h2>
           <blockquote>              
           <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
           <p>Hasta que extienda el círculo de su compasión a todos los seres vivos, el hombre no encontrará la paz</p>
