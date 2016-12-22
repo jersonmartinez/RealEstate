4d104ae7ef4456d4f54560df27df8777474e2144
Side Master <sidemasterdelfuturo@gmail.com>
2016-12-22 00:27:27 -0600

    Adding https

--- Desktop/html/build/view_desktop.php
+++ Desktop/html/build/view_desktop.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="side-body padding-top">
         <div class="row">
-			<center><iframe src="http://192.168.154.1/RealEstate/" width="98%" height="530"> </iframe></center>
+			<center><iframe src="https://bellyrealestate.com/new/" width="98%" height="530"> </iframe></center>
 			<?php include ("build/modals.php"); ?>
         </div>
     </div>
--- Desktop/html/build/view_mobile.php
+++ Desktop/html/build/view_mobile.php
@@ -4,13 +4,13 @@
         	<div class="row no-margin-bottom">
             	<div class="col-sm-3 col-xs-12">
                 	<div class="card card-success">
-						<center><iframe src="http://192.168.154.1/RealEstate/" width="100%" height="530"> </iframe></center>
+						<center><iframe src="https://bellyrealestate.com/new/" width="100%" height="530"> </iframe></center>
                 	</div>
                 </div>
 
                 <div class="col-sm-9 col-xs-12">
                 	<div class="card card-success">
-						<center><iframe src="http://192.168.154.1/RealEstate/" width="100%" height="530"> </iframe></center>
+						<center><iframe src="https://bellyrealestate.com/new/" width="100%" height="530"> </iframe></center>
                 	</div>
                 </div>
             </div>
