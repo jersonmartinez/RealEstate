d48ef30688e7f7945b503ce9ac0a2bef953c07e3
Side Master <sidemasterdelfuturo@gmail.com>
2016-12-21 21:00:58 -0600

    Fixed View All Messages Favorite and Fetch Array

--- Desktop/html/build/view_all_messages_favorite.php
+++ Desktop/html/build/view_all_messages_favorite.php
@@ -4,30 +4,32 @@
     
     $ConexM = $Conexion->query("SELECT * FROM sus_message WHERE favorite='1' ORDER BY id DESC;");
 
-    while ($CoM = $ConexM->fetch_array(MYSQLI_ASSOC)){
-        $GetImgArt = $Conexion->query("SELECT folder, src FROM publish_img WHERE id_art='".$CoM['id_art']."' LIMIT 1;")->fetch_array(MYSQLI_ASSOC);
-        ?>
-            <a href="#" onclick="LoadMessage(<?php echo $CoM['id']; ?>);">
-                <li>
-                    <img src="<?php echo "../".$GetImgArt['folder'].$GetImgArt['src']; ?>" width="60px" height="60px" class="profile-img pull-left">
-               
-                    <div class="message-block">
-                        <div><span class="username"><?php echo $CoM['fullname']; ?></span> <span class="message-datetime"><?php echo nicetime(date("Y-m-d H:i", $CoM['date_log_unix'])); ?></span>
-                        </div>
-                        <div class="message">
-                            <?php 
-                                echo substr($CoM['message'], 0, 260); 
+    if ($ConexM->num_rows > 0){
+        while ($CoM = $ConexM->fetch_array(MYSQLI_ASSOC)){
+            $GetImgArt = $Conexion->query("SELECT folder, src FROM publish_img WHERE id_art='".$CoM['id_art']."' LIMIT 1;")->fetch_array(MYSQLI_ASSOC);
+            ?>
+                <a href="#" onclick="LoadMessage(<?php echo $CoM['id']; ?>);">
+                    <li>
+                        <img src="<?php echo "../".$GetImgArt['folder'].$GetImgArt['src']; ?>" width="60px" height="60px" class="profile-img pull-left">
+                   
+                        <div class="message-block">
+                            <div><span class="username"><?php echo $CoM['fullname']; ?></span> <span class="message-datetime"><?php echo nicetime(date("Y-m-d H:i", $CoM['date_log_unix'])); ?></span>
+                            </div>
+                            <div class="message">
+                                <?php 
+                                    echo substr($CoM['message'], 0, 260); 
 
-                                if (strlen($CoM['message']) > 260){
-                                    echo "...";
-                                }
-                            ?>
+                                    if (strlen($CoM['message']) > 260){
+                                        echo "...";
+                                    }
+                                ?>
+                            </div>
                         </div>
-                    </div>
 
-                </li>
-            </a>
-        <?php
+                    </li>
+                </a>
+            <?php
+        }
     }
 ?>
 
