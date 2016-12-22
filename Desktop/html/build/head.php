49f4ecdc4b7053d208c88059bd7c740f36d490bc
Side Master <sidemasterdelfuturo@gmail.com>
2016-12-21 21:58:02 -0600

    Has been changed the path google fonts apis for https connection

--- Desktop/html/build/head.php
+++ Desktop/html/build/head.php
@@ -7,8 +7,8 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Fonts -->
-<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
 <!-- CSS Libs -->
 <link rel="stylesheet" type="text/css" href="../lib/css/bootstrap.min.css">
 
