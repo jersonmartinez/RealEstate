1ebca05809c3b581b57b1bff5b5f1242f64fa127
Side Master <sidemasterdelfuturo@gmail.com>
2016-12-21 19:25:22 -0600

    Credentials to connect to the server

--- Desktop/connect_server/connect_server.php
+++ Desktop/connect_server/connect_server.php
@@ -5,7 +5,8 @@
 		}
 	}
 
-	@$Conexion = new TheCodeBrain("127.0.0.1", "SideMaster", "Inform@tico", "re");
+	@$Conexion = new TheCodeBrain("mysql.hostinger.es", "u141201621_sm", "Windows10", "u141201621_re");
+	// @$Conexion = new TheCodeBrain("127.0.0.1", "SideMaster", "Inform@tico", "re");
 
 	if ($Conexion->connect_error)
 		$error = true;
